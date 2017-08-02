a33dde700cd3d35f2114ae09ea94d22ff7a01df4
Jayesh <jayesh@Jayeshs-iMac.local>
2017-08-02 23:26:52 +0530

    Initial Commit

---
+++ JSContactBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSContactBook
+//
+//  Created by Jayesh on 8/2/17.
+//  Copyright © 2017 Jayesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSContactBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSContactBook
+//
+//  Created by Jayesh on 8/2/17.
+//  Copyright © 2017 Jayesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
