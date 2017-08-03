4f7d8aba7ad653dca856f4fe59c3021870b20806
Jayesh <jsojitra@bosleo.com>
2017-08-03 18:36:49 +0530

    Added contact detail view controller and updated project folder structure.

---
+++ JSContactBook/Controller/ContactDetailViewController/ContactDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactDetailViewController.h
+//  JSContactBook
+//
+//  Created by Jayesh on 03/08/17.
+//  Copyright © 2017 Jayesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactDetailViewController : UIViewController
+
+@end
