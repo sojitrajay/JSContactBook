3e15f0c8ea187cb16bee27bb0187c4ea0dafd1f1
Jayesh <jayesh@Jayeshs-iMac.local>
2017-08-04 05:58:32 +0530

    Adding phone number addition and updating functionality.

---
+++ JSContactBook/Controller/ContactEditViewController/ContactEditAddDataTableViewCell/ContactEditAddDataTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ContactEditAddDataTableViewCell.h
+//  JSContactBook
+//
+//  Created by Jayesh on 8/4/17.
+//  Copyright © 2017 Jayesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactEditAddDataTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIButton *buttonAdd;
+@property (weak, nonatomic) IBOutlet UITextField *labelTitle;
+
+@end
---
+++ JSContactBook/Controller/ContactEditViewController/ContactEditAddDetailTableViewCell/ContactEditAddDetailTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ContactEditAddDetailTableViewCell.m
+//  JSContactBook
+//
+//  Created by Jayesh on 8/4/17.
+//  Copyright © 2017 Jayesh. All rights reserved.
+//
+
+#import "ContactEditAddDetailTableViewCell.h"
+
+@implementation ContactEditAddDetailTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
